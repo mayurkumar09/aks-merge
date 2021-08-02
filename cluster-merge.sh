839486ab993bc20ea0fd42df6a9e68f6c7fd3dfa
mayurdentsu <mayur.kumar@dentsu.com>
2021-08-02 11:08:31 +0530

    initial commit

---
+++ cluster-merge.sh
@@ -0,0 +1,22 @@
+init () {
+    echo "Welcome to sidnx"
+    auth=$(az login)
+}
+
+merge () {
+# Get all the subscrition on users account
+    subs=$(az account list --output tsv --query '[].[id]')
+    for sub in ${subs[@]}; do
+        az account set --subscription $sub
+        clusters=($(az aks list --output tsv --query '[].[name]'))
+        rg=($(az aks list --output tsv --query '[].[resourceGroup]'))
+        if [[ "${#clusters[@]}" -ne 0 && "${#rg[@]}" -ne 0 ]]; then
+            for (( i=0; i < "${#clusters[@]}"; i++ )); do
+                az aks get-credentials --admin --name "${clusters[$i]}" --resource-group "${rg[$i]}" --overwrite-existing
+            done
+        fi
+    done
+}
+
+init
+merge
